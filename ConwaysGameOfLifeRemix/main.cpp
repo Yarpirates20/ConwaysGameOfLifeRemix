77183c399e4f9de3fac9704632ec6cc682c95d95
Robert Samoraj <rsamoraj11@gmail.com>
2023-08-01 20:06:34 -0500

    Create render function to pretty print state

--- ConwaysGameOfLifeRemix/main.cpp
+++ ConwaysGameOfLifeRemix/main.cpp
@@ -3,32 +3,54 @@
 using namespace std;
 
 vector<vector<int>> randomState(int, int);
+void render(vector<vector<int>>);
 
 int main()
 {
+	srand(time(0));
+
 	int height = 10, width = 20;
 	vector<vector<int>> deadState = randomState(height, width);
-	for (int row = 0; row < height; row++)
-	{
-		for (int col = 0; col < width; col++)
-		{
-			cout << deadState[row][col];
-		}
-		cout << endl;
-	}
+	render(deadState);
+
 	return 0;
 }
 
 vector<vector<int>> randomState(int height, int width)
 {
 	vector<vector<int>> state(height, vector<int>(width));
+	
 
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
 		{
-			state[i][j] = 0;
+			int randomBit = rand() % 2;
+			state[i][j] = randomBit;
 		}
 	}
 	return state;
+}
+
+void render(vector<vector<int>> stateVec)
+{
+	int rowSize = stateVec.size();
+	int colSize = stateVec[0].size();
+
+	for (int row = 0; row < rowSize; row++)
+	{
+		for (int col = 0; col < colSize; col++)
+		{
+			if (stateVec[row][col] == 0)
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << "#";
+			}
+		}
+
+		cout << endl;
+	}
 }
