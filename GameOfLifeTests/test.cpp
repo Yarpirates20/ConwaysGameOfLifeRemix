6f32564370f8075627da97e41f10af283af7f707
Robert Samoraj <rsamoraj11@gmail.com>
2023-08-04 11:57:08 -0500

    Create test to check if live cells with 3+ neighbors become dead

--- GameOfLifeTests/test.cpp
+++ GameOfLifeTests/test.cpp
@@ -12,16 +12,54 @@ TEST(SquareTest, square)
 	EXPECT_EQ(25, square(5));
 }
 
-TEST(DeadCellsTest, nextBoardState)
-{
-	int rows = 5, cols = 5;
-	std::vector<std::vector<int>> dead(rows, std::vector<int>(cols, 0));
-	EXPECT_EQ(dead, nextBoardState(dead));
-}
+//class boardState : public ::testing::Test
+//{
+//protected:
+//	void SetUp() override
+//	{
+//		
+//	}
+//
+//	std::vector<std::vector<int>> deadState = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
+//	std::vector<std::vector<int>> initState = { {0, 0, 1}, {0, 1, 1}, {0, 0, 0} };
+//
+//};
 
-TEST(DeadToAliveTest, nextBoardState)
+namespace boardState
 {
-	std::vector<std::vector<int>> initState = { {0, 0, 1}, {0, 1, 1}, {0, 0, 0} };
-	std::vector<std::vector<int>> next = { {0, 1, 1}, {0, 1, 1}, {0, 0, 0} };
-	EXPECT_EQ(next, nextBoardState(initState));
+	class boardStateTest : public ::testing::Test
+	{
+	protected:
+		void SetUp() override
+		{
+
+		}
+
+		std::vector<std::vector<int>> deadState_ = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
+		std::vector<std::vector<int>> initState_ = { {0, 0, 1}, {0, 1, 1}, {0, 0, 0} };
+		std::vector<std::vector<int>> livingState_ = { {1, 1, 1}, {1, 1, 1}, {1, 1, 1} };
+		
+
+	};
+	
+	TEST_F(boardStateTest, DeadStaysDead)
+	{
+		/*int rows = 5, cols = 5;
+		std::vector<std::vector<int>> dead(rows, std::vector<int>(cols, 0));*/
+		EXPECT_EQ(deadState_, nextBoardState(deadState_));
+		SUCCEED();
+	}
+
+	TEST_F(boardStateTest, DeadToAlive)
+	{
+		/*std::vector<std::vector<int>> initState = { {0, 0, 1}, {0, 1, 1}, {0, 0, 0} };*/
+		std::vector<std::vector<int>> next = { {0, 1, 1}, {0, 1, 1}, {0, 0, 0} };
+		EXPECT_EQ(next, nextBoardState(initState_));
+	}
+
+	TEST_F(boardStateTest, AliveToDead)
+	{
+		std::vector<std::vector<int>> next = { {1, 0, 1}, {0, 0, 0}, {1, 0, 1} };
+		EXPECT_EQ(next, nextBoardState(livingState_));
+	}
 }
